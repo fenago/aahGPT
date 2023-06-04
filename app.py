2223e97fe1498ea1e495f4195f4b1faa689751c7
Dr. Ernesto Lee <socrates73@gmail.com>
2023-06-04 12:30:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ openai_api_key = st.secrets["openai"]["api_key"]
 index_name = "influential-americans"
 
 # Define a simple Streamlit app
-st.title("Ask aahGPT about African American History")
+st.title("Ask Llama about African American History")
 query = st.text_input("What would you like to ask?", "")
 
 # If the 'Submit' button is clicked
@@ -20,23 +20,22 @@ if st.button("Submit"):
     if not query.strip():
         st.error(f"Please provide the search query.")
     else:
-        try:
-            # Initialize Pinecone
-            pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment)
+        # Initialize Pinecone
+        pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment)
 
-            # Check if the index exists
-            if index_name not in pinecone.list_indexes():
-                st.error(f"The index '{index_name}' does not exist.")
-                return
+        # Check if the index exists
+        if index_name not in pinecone.list_indexes():
+            st.error(f"The index '{index_name}' does not exist.")
+        else:
+            try:
+                # This example uses gpt-4 by default; feel free to change if desired
+                llm_predictor = LLMPredictor(llm=OpenAI(api_key=openai_api_key, temperature=0, model_name="gpt-4"))
 
-            # This example uses gpt-4 by default; feel free to change if desired
-            llm_predictor = LLMPredictor(llm=OpenAI(api_key=openai_api_key, temperature=0, model_name="gpt-4"))
+                # Connect to the existing Pinecone index
+                index = pinecone.Index(index_name)
 
-            # Connect to the existing Pinecone index
-            index = pinecone.Index(index_name)
-
-            # Query and retrieve the response
-            query_result = index.query(queries=[query], top_k=10)
-            st.success(query_result)
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
+                # Query and retrieve the response
+                query_result = index.query(queries=[query], top_k=10)
+                st.success(query_result)
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
