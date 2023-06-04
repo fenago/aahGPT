337692bfa215371f057921ea2411731758c21b5c
Dr. Ernesto Lee <socrates73@gmail.com>
2023-06-04 12:28:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,42 @@
-import streamlit as st
+import os, streamlit as st
 import pinecone
+from langchain.llms.openai import OpenAI
+from llama_index import LLMPredictor, ServiceContext
 
-# Get Pinecone API key and environment from Streamlit secrets
+# Streamlit Secrets
 pinecone_api_key = st.secrets["pinecone"]["api_key"]
 pinecone_environment = st.secrets["pinecone"]["environment"]
-
-# Authenticate with Pinecone
-pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment)
+openai_api_key = st.secrets["openai"]["api_key"]
 
 # Pinecone index name
 index_name = "influential-americans"
 
-# Check if the index exists
-if index_name in pinecone.list_indexes():
-    st.write(f"The index '{index_name}' exists.")
-else:
-    st.write(f"The index '{index_name}' does not exist.")
+# Define a simple Streamlit app
+st.title("Ask aahGPT about African American History")
+query = st.text_input("What would you like to ask?", "")
 
-# Create an interface to query the index
-query_input = st.text_input("Enter your query:")
+# If the 'Submit' button is clicked
+if st.button("Submit"):
+    if not query.strip():
+        st.error(f"Please provide the search query.")
+    else:
+        try:
+            # Initialize Pinecone
+            pinecone.init(api_key=pinecone_api_key, environment=pinecone_environment)
 
-if query_input:
-    # Create a Pinecone index instance
-    index = pinecone.Index(index_name)
-    
-    # Query the index
-    query_result = index.query(queries=[query_input], top_k=10)
-    
-    # Show the result
-    for item in query_result.results:
-        st.write(item.id, item.score)
-else:
-    st.write("Please enter a query.")
+            # Check if the index exists
+            if index_name not in pinecone.list_indexes():
+                st.error(f"The index '{index_name}' does not exist.")
+                return
+
+            # This example uses gpt-4 by default; feel free to change if desired
+            llm_predictor = LLMPredictor(llm=OpenAI(api_key=openai_api_key, temperature=0, model_name="gpt-4"))
+
+            # Connect to the existing Pinecone index
+            index = pinecone.Index(index_name)
+
+            # Query and retrieve the response
+            query_result = index.query(queries=[query], top_k=10)
+            st.success(query_result)
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
