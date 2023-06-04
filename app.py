37ac44d5145d5647913ac9a25dee1aa37cab6cc1
Dr. Ernesto Lee <socrates73@gmail.com>
2023-06-04 11:51:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,31 @@
+# Import necessary libraries
 import streamlit as st
 import pinecone
-import json
 
-# Pinecone configuration
-pinecone_index_name = "influential-americans"
+# Get the Pinecone API key from Streamlit secrets
+pinecone_api_key = st.secrets["pinecone_api_key"]
 
-# Connect to the Pinecone index
-pinecone.init(api_key=st.secrets["pinecone_api_key"])
-pinecone_index = pinecone.Index(index_name=pinecone_index_name)
+# Initialize Pinecone
+pinecone.init(api_key=pinecone_api_key)
 
-def main():
-    st.title("Influential African Americans Search")
+# Pinecone index name
+index_name = "influential-americans"
+
+# Check if the index exists
+if index_name in pinecone.list_indexes():
+    st.write(f"The index '{index_name}' exists.")
+else:
+    st.write(f"The index '{index_name}' does not exist.")
+
+# Create an interface to query the index
+query_input = st.text_input("Enter your query:")
+
+if query_input:
+    # Create a pinecone query
+    query_result = pinecone.deployment(index_name).query(queries=[query_input], top_k=10)
     
-    query = st.text_input("Enter a query")
-    
-    if query:
-        response = search_influential_americans(query)
-        format_output(response)
-
-def search_influential_americans(query):
-    try:
-        response = pinecone_index.query(queries=[query], top_k=5)
-        return response
-    except pinecone.ApiException as e:
-        st.error(f"An error occurred during the query: {e}")
-        return None
-
-def format_output(response):
-    if not response or not response.ids:
-        st.error("I am sorry, but your query is outside the boundaries of what I was trained on.")
-    else:
-        st.subheader("Response:")
-        st.write(response)
-        # Add any other desired formatting of the response
-
-if __name__ == "__main__":
-    main()
+    # Show the result
+    for item in query_result.results:
+        st.write(item.id, item.score)
+else:
+    st.write("Please enter a query.")
