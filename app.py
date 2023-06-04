620c2facbf7e32946986d4f0df6f37251e708ad9
Dr. Ernesto Lee <socrates73@gmail.com>
2023-06-04 10:57:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from llama_index import GPTVectorStoreIndex
 from llama_index.vector_stores import PineconeVectorStore
-import os
 
 # Pinecone configuration
 pinecone_index_name = "influential-americans"
@@ -16,8 +15,8 @@ vector_store = PineconeVectorStore(
     environment=pinecone_index_environment
 )
 
-# Create GPTVectorStoreIndex
-city_index = GPTVectorStoreIndex(vector_store=vector_store)
+# Create an empty GPTVectorStoreIndex with the provided index structure
+aa_index = GPTVectorStoreIndex(index_struct=vector_store.index_struct)
 
 def main():
     st.title("Influential African Americans Search")
