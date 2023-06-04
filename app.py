eb211ee472c592736e8fbeacf7b0f01b80b4e9bc
Dr. Ernesto Lee <socrates73@gmail.com>
2023-06-04 11:02:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
 import streamlit as st
 import os
-from llama_index import GPTVectorStoreIndex
-from llama_index.vector_stores import PineconeVectorStore
+import pinecone
 
 # Pinecone configuration
 pinecone_index_name = "influential-americans"
-pinecone_index_environment = "asia-southeast1-gcp"  # Update with your Pinecone environment
 
 # Set Pinecone API key
 os.environ["PINECONE_API_KEY"] = "YOUR_PINECONE_API_KEY"  # Update with your Pinecone API key
 
-# Connect to the existing Pinecone index
-vector_store = PineconeVectorStore(
-    index_name=pinecone_index_name,
-    environment=pinecone_index_environment
-)
-
-# Create an empty GPTVectorStoreIndex
-city_index = GPTVectorStoreIndex()
+# Connect to the Pinecone index
+pinecone.init(api_key=os.environ["PINECONE_API_KEY"])
+pinecone_index = pinecone.Index(index_name=pinecone_index_name)
 
 def main():
     st.title("Influential African Americans Search")
@@ -25,17 +18,24 @@ def main():
     query = st.text_input("Enter a query")
     
     if query:
-        response = city_index.query(query)
+        response = search_influential_americans(query)
         format_output(response)
 
+def search_influential_americans(query):
+    try:
+        response = pinecone_index.query(queries=[query], top_k=5)
+        return response
+    except pinecone.ApiException as e:
+        st.error(f"An error occurred during the query: {e}")
+        return None
+
 def format_output(response):
-    if not response or not response.get_formatted_sources():
+    if not response or not response.ids:
         st.error("I am sorry, but your query is outside the boundaries of what I was trained on.")
     else:
         st.subheader("Response:")
         st.write(response)
-        st.subheader("Formatted Sources:")
-        st.write(response.get_formatted_sources())
+        # Add any other desired formatting of the response
 
 if __name__ == "__main__":
     main()
